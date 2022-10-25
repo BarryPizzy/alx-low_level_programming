1e45d9924644133ea386c46956bb44a1a4e60299
BarryPizzy <barilugbenepeter025@gmail.com>
2022-10-25 14:38:06 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print string followed by a new line to stdout
+ * @str: string to be printed
+ * Return: string and a new line
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i] != '\0'; i++)
+	}
+	_putchar('\n');
+}
